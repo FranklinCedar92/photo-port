2c59259b25bc6c6da1828a1443e3d05fc59c29af
Franklin <franklin.c.brown@gmail.com>
2022-05-05 06:15:06 -0700

    code overhaul: Lesson 20.4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
-import Gallery from "./components/Gallery";
+import About from './components/About';
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -16,20 +17,27 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        >
-      </Nav>
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
-      </main> 
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
+      </main>
     </div>
   );
 }
